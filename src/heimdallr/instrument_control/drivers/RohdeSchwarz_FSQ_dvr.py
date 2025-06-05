c188dc4816a05384c8028c5222b1b4b10e20be46
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-06-05 15:48:46 -0600

    Added state tracker

--- src/heimdallr/instrument_control/drivers/RohdeSchwarz_FSQ_dvr.py
+++ src/heimdallr/instrument_control/drivers/RohdeSchwarz_FSQ_dvr.py
@@ -17,24 +17,27 @@ class RohdeSchwarzFSQ(SpectrumAnalyzerCtg):
 		self.trace_lookup = {}
 	
 	def set_freq_start(self, f_Hz:float):
+		self.modify_state(self.get_freq_start, SpectrumAnalyzerCtg.FREQ_START, f_Hz)
 		self.write(f"SENS:FREQ:STAR {f_Hz} Hz")
 	def get_freq_start(self):
-		return float(self.query(f"SENS:FREQ:STAR?"))
+		return self.modify_state(None, SpectrumAnalyzerCtg.FREQ_START, float(self.query(f"SENS:FREQ:STAR?")))
 	
 	def set_freq_end(self, f_Hz:float):
+		self.modify_state(self.get_freq_end, SpectrumAnalyzerCtg.FREQ_END, f_Hz)
 		self.write(f"SENS:FREQ:STOP {f_Hz}")
 	def get_freq_end(self,):
-		return float(self.query(f"SENS:FREQ:STOP?"))
+		return self.modify_state(None, SpectrumAnalyzerCtg.FREQ_END, float(self.query(f"SENS:FREQ:STOP?")))
 	
 	def set_ref_level(self, ref_dBm:float):
 		ref_dBm = max(-130, min(ref_dBm, 30))
 		if ref_dBm != ref_dBm:
 			self.log.error(f"Did not apply command. Instrument limits values from -130 to 30 dBm and this range was violated.")
 			return
+		self.modify_state(self.get_ref_level, SpectrumAnalyzerCtg.REF_LEVEL, ref_dBm)
 		self.write(f"CALC:UNIT:POW dBm") # Set units to DBM (Next command refers to this unit)
 		self.write(f"DISP:WIND:TRAC:Y:RLEV {ref_dBm}")
 	def get_ref_level(self):
-		return float(self.query("DISP:WIND:TRAC:Y:RLEV?"))
+		return self.modify_state(None, SpectrumAnalyzerCtg.REF_LEVEL, float(self.query("DISP:WIND:TRAC:Y:RLEV?")))
 	
 	def set_y_div(self, step_dB:float):
 		
@@ -43,21 +46,24 @@ class RohdeSchwarzFSQ(SpectrumAnalyzerCtg):
 			self.log.error(f"Did not apply command. Instrument limits values from 1 to 20 dB and this range was violated.")
 			return
 		
+		self.modify_state(self.get_y_div, SpectrumAnalyzerCtg.Y_DIV, step_dB)
 		full_span_dB = step_dB*10 #Sets total span, not per div, so must multiply by num. divisions (10)
 		self.write(f":DISP:WIND:TRAC:Y:SCAL {full_span_dB} DB") 
 	def get_y_div(self):
 		full_span_dB = float(self.query(f":DISP:WIND:TRAC:Y:SCAL?"))
-		return full_span_dB/10
+		return self.modify_state(None, SpectrumAnalyzerCtg.Y_DIV, full_span_dB/10)
 	
-	def set_res_bandwidth(self, rbw_Hz:float, channel:int=1):
+	def set_res_bandwidth(self, rbw_Hz:float):
+		self.modify_state(self.get_res_bandwidth, SpectrumAnalyzerCtg.RES_BW, rbw_Hz)
 		self.write(f"SENS:BAND:RES {rbw_Hz} Hz")
-	def get_res_bandwidth(self, channel:int=1):
-		return float(self.query(f"SENS:BAND:RES?"))
+	def get_res_bandwidth(self):
+		return self.modify_state(None, SpectrumAnalyzerCtg.RES_BW, float(self.query(f"SENS:BAND:RES?")))
 	
 	def set_continuous_trigger(self, enable:bool):
+		self.modify_state(self.get_continuous_trigger, SpectrumAnalyzerCtg.CONTINUOUS_TRIG_EN, enable)
 		self.write(f"INIT:CONT {bool_to_ONFOFF(enable)}")
 	def get_continuous_trigger(self):
-		return str_to_bool(self.query(f"INIT:CONT?"))
+		return self.modify_state(None, SpectrumAnalyzerCtg.CONTINUOUS_TRIG_EN, str_to_bool(self.query(f"INIT:CONT?")))
 	
 	def send_manual_trigger(self, send_cls:bool=True):
 		if send_cls:
@@ -144,6 +150,8 @@ class RohdeSchwarzFSQ(SpectrumAnalyzerCtg):
 		f_list = list(np.linspace(self.get_freq_start(), self.get_freq_end(), len(float_data)))
 		
 		out_data = {'x':f_list, 'y':float_data, 'x_units':'Hz', 'y_units':'dBm'}
+		
+		self.modify_state(None, SpectrumAnalyzerCtg.TRACE_DATA, out_data, channel=trace)
 		
 		# Convert Y-unit to dBm
 		return out_data
