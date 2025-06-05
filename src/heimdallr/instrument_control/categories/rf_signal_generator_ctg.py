de7fef3a6c0dfc227770573397a34f45747f0b3e
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-06-05 16:26:12 -0600

    Update rf_signal_generator_ctg.py

--- src/heimdallr/instrument_control/categories/rf_signal_generator_ctg.py
+++ src/heimdallr/instrument_control/categories/rf_signal_generator_ctg.py
@@ -2,7 +2,7 @@ from heimdallr.base import *
 
 class RFSignalGeneratorCtg0(Driver):
 	
-	def __init__(self, address:str, log:LogPile, expected_idn:str="", **kwargs):
+	def __init__(self, address:str, log:plf.LogPile, expected_idn:str="", **kwargs):
 		super().__init__(address, log, expected_idn=expected_idn, **kwargs)
 
 class RFSignalGeneratorCtg1(Driver):
@@ -11,18 +11,35 @@ class RFSignalGeneratorCtg1(Driver):
 	FREQ = "freq[Hz]"
 	ENABLE = "enable[bool]"
 	
-	def __init__(self, address:str, log:LogPile, expected_idn:str="", **kwargs):
+	def __init__(self, address:str, log:plf.LogPile, expected_idn:str="", **kwargs):
 		super().__init__(address, log, expected_idn=expected_idn, **kwargs)
 		
 		self.state[RFSignalGeneratorCtg1.POWER] = None
 		self.state[RFSignalGeneratorCtg1.FREQ] = None
 		self.state[RFSignalGeneratorCtg1.ENABLE] = None
 	
+	@abstractmethod
 	def set_power(self, p_dBm:float):
 		pass
+	@abstractmethod
+	def get_power(self):
+		pass
 	
+	@abstractmethod
 	def set_freq(self, f_Hz:float):
 		pass
-	
-	def set_enable_rf(self,enable:bool):
+	@abstractmethod
+	def get_freq(self):
 		pass
+	
+	@abstractmethod
+	def set_enable_rf(self, enable:bool):
+		pass
+	@abstractmethod
+	def get_enable_rf(self):
+		pass
+	
+	def refresh_all(self):
+		self.get_power()
+		self.get_freq()
+		self.get_enable_rf()
