b35bafb7630f6bc817f27123f48ac092fea73a30
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-06-06 10:11:17 -0600

    Flesh out temp controller base class

--- src/heimdallr/instrument_control/categories/temperature_controller_ctg.py
+++ src/heimdallr/instrument_control/categories/temperature_controller_ctg.py
@@ -5,11 +5,25 @@ class PIDTemperatureControllerCtg(Driver):
 	def __init__(self, address:str, log:plf.LogPile, expected_idn:str=""):
 		super().__init__(address, log, expected_idn=expected_idn)
 	
-	def set_temp(self, temp_K:float, channel:int=1):
+	@abstractmethod
+	def set_setpoint(self, temp_K:float, channel:int=1):
+		pass
+	@abstractmethod
+	def get_setpoint(self, channel:int=1):
 		pass
 	
-	def get_temp(self, temp_K:float, channel:int=1):
+	@abstractmethod
+	def get_temp(self, channel:int=1):
 		pass
 	
+	@abstractmethod
+	def set_pid(self, P:float, I:float, D:float, channel:int=1):
+		pass
+	
+	@abstractmethod
+	def get_pid(self, channel:int=1):
+		pass
+	
+	@abstractmethod
 	def set_enable(self, enable:bool, channel:int=1):
 		pass
