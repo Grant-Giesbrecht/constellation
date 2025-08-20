d648a357d216abd6c61a5d7a3ff137ec51f3ac8c
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-08-20 16:43:29 -0600

    save_sparam: Added comments

--- src/constellation/instrument_control/vector_network_analyzer/tools/save_sparam.py
+++ src/constellation/instrument_control/vector_network_analyzer/tools/save_sparam.py
@@ -23,11 +23,12 @@ trc_s12 = zva.find_trace(BasicVectorNetworkAnalyzerCtg.MEAS_S12)
 trc_s21 = zva.find_trace(BasicVectorNetworkAnalyzerCtg.MEAS_S21)
 trc_s22 = zva.find_trace(BasicVectorNetworkAnalyzerCtg.MEAS_S22)
 
+# Check if any required traces were not found
 if None in [trc_s11, trc_s12, trc_s21, trc_s22]:
 	print(f"Failed to find one or more required traces. Aborting.")
 	sys.exit()
 	
-# Read traces
+# Read trace data
 td_s11 = zva.get_trace_data(trc_s11)
 td_s22 = zva.get_trace_data(trc_s22)
 td_s12 = zva.get_trace_data(trc_s12)
@@ -38,10 +39,10 @@ sp_data = {"S11": td_s11, "S22":td_s22, "S12":td_s12, "S21":td_s21}
 file_info = {"cal_notes":cal_notes, "gen_notes":other_notes, "timestamp":datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}
 data_out = {"data":sp_data, "info":file_info}
 
+# Save to disk
 dict_to_hdf(data_out, FILENAME)
 
-
-
+# Create S-parameter plot
 plot_vna_mag(sp_data['S11'], label="S11")
 plot_vna_mag(sp_data['S22'], label="S22")
 plot_vna_mag(sp_data['S21'], label="S21")
