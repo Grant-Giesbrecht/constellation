5d0f16516cbe8aa7d7715bac60d97f7ed87c8bab
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2026-01-23 08:08:51 -0700

    Added check_online

--- src/constellation/helpers.py
+++ src/constellation/helpers.py
@@ -1,2 +1,8 @@
 from constellation.base import *
 
+def check_online(dvr:Driver, name:str, log:plf.LogPile):
+	if dvr.online:
+		log.info(f"{name} >ONLINE<.")
+	else:
+		log.info(f"Failed to connect to {name}.")
+		exit()
