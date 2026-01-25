e2e75960cb59e4ca31434875ea02333e7dc39198
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2026-01-24 17:08:25 -0700

    Properly implemented AWG cat

--- src/constellation/instrument_control/arb_waveform_generator/arb_waveform_generator_ctg.py
+++ src/constellation/instrument_control/arb_waveform_generator/arb_waveform_generator_ctg.py
@@ -1,10 +1,42 @@
 from constellation.base import *
 from constellation.networking.net_client import *
 
+class AWGChannelState(InstrumentState):
+	
+	__state_fields__ = ("waveform_type", "frequency", "amplitude", "offset", "output_enable")
+	
+	def __init__(self, log:plf.LogPile=None):
+		super().__init__(log=log)
+		
+		self.add_param("waveform_type", unit="")
+		self.add_param("frequency", unit="Hz")
+		self.add_param("amplitude", unit="Vpp")
+		self.add_param("offset", unit="V")
+		self.add_param("output_enable", unit="Hz")
+		
+		self.validate()
+
+class ArbitraryWaveformGeneratorState(InstrumentState):
+	
+	__state_fields__ = ("first_channel", "num_channels", "channels")
+	
+	def __init__(self, log:plf.LogPile=None, first_channel:int=1, num_channels:int=2):
+		super().__init__(log=log)
+		
+		self.add_param("first_channel", unit="1", value=first_channel)
+		self.add_param("num_channels", unit="1", value=num_channels)
+		
+		self.add_param("channels", unit="", value=IndexedList(self.first_channel, self.num_channels, validate_type=AWGChannelState, log=log))
+		
+		for ch_no in self.channels.get_range():
+			self.channels[ch_no] = AWGChannelState(log=log)
+		
+		self.validate()
+
 class ArbitraryWaveformGenerator(Driver):
 	
 	WAVE_SINE = "wave-sine"
-	WAVE_SQUARE = "wave-sine"
+	WAVE_SQUARE = "wave-square"
 	WAVE_RAMP = "wave-ramp"
 	WAVE_PULSE = "wave-pulse"
 	WAVE_NOISE = "wave-noise"
@@ -12,15 +44,144 @@ class ArbitraryWaveformGenerator(Driver):
 	WAVE_DC = "wave-dc"
 	
 	def __init__(self, address:str, log:plf.LogPile, relay:CommandRelay, expected_idn:str="", dummy:bool=False, max_channels:int=2):
-		super().__init__(address, log, expected_idn=expected_idn, dummy=dummy, relay=relay)
+		
+		_state = ArbitraryWaveformGeneratorState(log, 1, max_channels)
+		super().__init__(address, log, relay, _state, expected_idn=expected_idn, dummy=dummy)
 		
 		self.max_channels = max_channels
+		
+		if self.dummy:
+			self.init_dummy_state()
 	
-	def apply_state(self, new_state: dict):
-		pass
+	def init_dummy_state(self):
+		
+		for ch_no in self.channels.get_range():
+			self.set_waveform(ch_no, self.WAVE_SINE)
+			self.set_frequency(ch_no, 1e3)
+			self.set_amplitude(ch_no, 1)
+			self.set_offset(ch_no, 0)
+			self.set_output_enable(ch_no, False)
+	
+	def dummy_responder(self, func_name:str, *args, **kwargs):
+		''' Function expected to behave as the "real" equivalents. ie. write commands don't
+		need to return anything, reads commands or similar should. What is returned here
+		should mimic what would be returned by the "real" function if it were connected to
+		hardware.
+		'''
+		
+		# Put everything in a try-catch in case arguments are missing or similar
+		try:
+			
+			# Check for known functions
+			found = True
+			adjective = ""
+			match func_name:
+				case "set_waveform":
+					rval = None
+				case "get_waveform":
+					rval = self.state.get(["channels", "waveform_type"], indices=[args[0]])
+				case "set_frequency":
+					rval = None
+				case "get_frequency":
+					rval = self.state.get(["channels", "frequency"], indices=[args[0]])
+				case "set_amplitude":
+					rval = None
+				case "get_amplitude":
+					rval = self.state.get(["channels", "amplitude"], indices=[args[0]])
+				case "set_offset":
+					rval = None
+				case "get_offset":
+					rval = self.state.get(["channels", "offset"], indices=[args[0]])
+				case "set_output_enable":
+					rval = None
+				case "get_output_enable":
+					rval = self.state.get(["channels", "output_enable"], indices=[args[0]])
+				case _:
+					found = False
+				
+			
+			# If function was found, label as recognized, else check match for general getter or setter
+			if found:
+				adjective = "recognized"
+			else:
+				if "set_" == func_name[:4]:
+					rval = -1
+					adjective = "set_"
+				elif "get_" == func_name[:4]:
+					rval = None
+					adjective = "get_"
+				else:
+					rval = None
+					adjective = "unrecognized"
+				
+			self.debug(f"Dummy responder sending >{protect_str(rval)}< to {adjective} function (>{func_name}<).")
+			return rval
+		except Exception as e:
+			self.error(f"Failed to respond to dummy instruction. ({e})")
+			return None
+	
+	@abstractmethod
+	def set_waveform(self, channel:int, wave:str):
+		self.modify_state(lambda: self.get_waveform(channel), ["channels", "waveform_type"], wave, indices=[channel])
+	
+	@abstractmethod
+	@enabledummy
+	def get_waveform(self, channel:int):
+		return self.modify_state(None, ["channels", "waveform_type"], self._super_hint, indices=[channel])
+	
+	@abstractmethod
+	def set_frequency(self, channel:int, freq_hz:float):
+		self.modify_state(lambda: self.get_frequency(channel), ["channels", "frequency"], freq_hz, indices=[channel])
+	
+	@abstractmethod
+	@enabledummy
+	def get_frequency(self, channel:int):
+		return self.modify_state(None, ["channels", "frequency"], self._super_hint, indices=[channel])
+	
+	@abstractmethod
+	def set_amplitude(self, channel:int, amplitude_Vpp:float):
+		self.modify_state(lambda: self.get_amplitude(channel), ["channels", "amplitude"], amplitude_Vpp, indices=[channel])
+	
+	@abstractmethod
+	@enabledummy
+	def get_amplitude(self, channel:int):
+		return self.modify_state(None, ["channels", "amplitude"], self._super_hint, indices=[channel])
+	
+	@abstractmethod
+	def set_offset(self, channel:int, offset_V:float):
+		self.modify_state(lambda: self.get_offset(channel), ["channels", "offset"], offset_V, indices=[channel])
+	
+	@abstractmethod
+	@enabledummy
+	def get_offset(self, channel:int):
+		return self.modify_state(None, ["channels", "offset"], self._super_hint, indices=[channel])
+	
+	@abstractmethod
+	def set_output_enable(self, channel:int, enable:bool):
+		self.modify_state(lambda: self.get_output_enable(channel), ["channels", "output_enable"], enable, indices=[channel])
+	
+	@abstractmethod
+	@enabledummy
+	def get_output_enable(self, channel:int):
+		return self.modify_state(None, ["channels", "output_enable"], self._super_hint, indices=[channel])
+	
+	def apply_state(self):
+		
+		for ch_no in self.channels.get_range():
+			self.set_waveform(ch_no, self.state.get(["channels", "waveform_type"], indices=[ch]))
+			self.set_frequency(ch_no, self.state.get(["channels", "frequency"], indices=[ch]))
+			self.set_amplitude(ch_no, self.state.get(["channels", "amplitude"], indices=[ch]))
+			self.set_offset(ch_no, self.state.get(["channels", "offset"], indices=[ch]))
+			self.set_output_enable(ch_no, self.state.get(["channels", "output_enable"], indices=[ch]))
 	
 	def refresh_state(self):
-		pass
+		
+		for ch_no in self.channels.get_range():
+			self.get_waveform(ch_no)
+			self.get_frequency(ch_no)
+			self.get_amplitude(ch_no)
+			self.get_offset(ch_no)
+			self.get_output_enable(ch_no)
 		
 	def refresh_data(self):
 		pass
--- src/constellation/instrument_control/arb_waveform_generator/drivers/Siglent_SDG2000X_dvr.py
+++ src/constellation/instrument_control/arb_waveform_generator/drivers/Siglent_SDG2000X_dvr.py
@@ -13,7 +13,7 @@ class SiglentSDG2000X(ArbitraryWaveformGenerator):
 	def __init__(self, address:str, log:plf.LogPile):
 		super().__init__(address, log, relay=DirectSCPIRelay(), expected_idn="Siglent Technologies, SDG2", max_channels=2)
 	
-	# @superreturn
+	@superreturn
 	def set_waveform(self, channel:int, wave:str):
 		
 		if wave == ArbitraryWaveformGenerator.WAVE_DC:
@@ -37,13 +37,100 @@ class SiglentSDG2000X(ArbitraryWaveformGenerator):
 		# Send command to instrument
 		self.write(f"C{channel}:BSWV WVTP,{code}")
 	
-	def set_offset(self, channel:int, offset_V:float):
+	def _refresh_wave_parameters(self, channel:int):
+		response_str = self.query(f"C{channel}:BSWV?")
+		# Response string will follow format:
+		# 'C1:BSWV WVTP,SINE,FRQ,1000HZ,PERI,0.001S,AMP,2.775V,AMPVRMS,0.980963Vrms,OFST,0V,HLEV,1.3875V,LLEV,-1.3875V,PHSE,0\n'
 		
-		#TODO: Error check offset level
+		# Split at all commas
+		tokens = response_str.split(',')
+		# Example result:
+		# ['C1:BSWV WVTP', <- 0
+		#  'SINE',
+		#  'FRQ',
+		#  '1000HZ', <- 3
+		#  'PERI',
+		#  '0.001S',
+		#  'AMP',
+		#  '2.775V', <- 7
+		#  'AMPVRMS',
+		#  '0.980963Vrms', <- 9
+		#  'OFST',
+		#  '0V', <- 11
+		#  'HLEV',
+		#  '1.3875V',
+		#  'LLEV',
+		#  '-1.3875V',
+		#  'PHSE',
+		#  '0\n']
+		
+		#
+		try:
+			wave_type = tokens[1]
+			self.state.channels[channel].waveform_type = wave_type
+			
+			freq_str = tokens[3]
+			self.state.channels[channel].frequency = float(freq_str[:-2])
+			
+			ampl_str = tokens[7]
+			self.state.channels[channel].amplitude = float(ampl_str[:-1])
+			
+			offs_str = tokens[11]
+			self.state.channels[channel].offset = float(offs_str[:-1])
+		except Exception as e:
+			pass
+	
+	@superreturn
+	def get_waveform(self, channel):
+		self._refresh_wave_parameters(channel)
+		self._super_hint = self.state.channels[channel].waveform_type
+	
+	@superreturn
+	def set_frequency(self, channel:int, freq_hz:float):
+		self.write(f"C{channel}:BSWV FRQ,{freq_hz}")
+	
+	@superreturn
+	def get_frequency(self, channel:int):
+		self._refresh_wave_parameters(channel)
+		self._super_hint = self.state.channels[channel].frequency
+	
+	@superreturn
+	def set_amplitude(self, channel:int, amplitude_Vpp:float):
+		self.write(f"C{channel}:BSWV AMP,{amplitude_Vpp}")
+	
+	@superreturn
+	def get_amplitude(self, channel:int):
+		self._refresh_wave_parameters(channel)
+		self._super_hint = self.state.channels[channel].amplitude
+	
+	@superreturn
+	def set_offset(self, channel:int, offset_V:float):
 		
 		# Send command to instrument
 		self.write(f"C{channel}:BSWV OFST,{offset_V}")
 	
-	def set_enable(self, channel:int, enable:bool):
-		
+	@superreturn
+	def get_offset(self, channel:int):
+		self._refresh_wave_parameters(channel)
+		self._super_hint = self.state.channels[channel].offset
+	
+	@superreturn
+	def set_output_enable(self, channel:int, enable:bool):
 		self.write(f"C{channel}:OUTP {bool_to_ONOFF(enable)}")
+	
+	@superreturn
+	def get_output_enable(self, channel:int):
+		response_str = self.query(f"C{channel}:OUTP?")
+		# Example output: 'C1:OUTP OFF,LOAD,HZ,PLRT,NOR\n'
+		
+		tokens = response_str.split(',')
+		bool_words = tokens[0].split(' ')
+		self._super_hint = str_to_bool(bool_words[1])
+	
+	def refresh_state(self):
+		''' Because this model queries all parameters at once, refresh_state 
+		is overridden.
+		'''
+		
+		for ch_no in self.channels.get_range():
+			self._refresh_wave_parameters(ch_no)
